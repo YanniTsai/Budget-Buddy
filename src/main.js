10fbd63c4cbd0fbfdb23266a94cf1847754b2acb
Yanni Tsai <loveyanni88@gmail.com>
2023-11-19 21:37:23 +0800

    import font awesome

--- src/main.js
+++ src/main.js
@@ -3,4 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(far, fas)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(store)
+app.use(router)
+app.mount('#app')
