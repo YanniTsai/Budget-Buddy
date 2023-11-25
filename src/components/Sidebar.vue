e4840dfd0ecf229b2c99d73ffe2bf9d862eaa88c
Yanni Tsai <loveyanni88@gmail.com>
2023-11-25 15:02:37 +0800

    feat: sidebar layout of desktop

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="sidebar d-flex flex-column justify-content-end align-items-center p-2">
+        <router-link to="/dashboard/account" class="d-flex flex-column align-items-center mb-2">
+            <i class="bi bi-person-fill"></i>
+            <div>帳戶管理</div>
+        </router-link>
+        <router-link to="/dashboard/setting" class="d-flex flex-column align-items-center mb-2">
+            <i class="bi bi-wrench"></i>
+            <div>系統設定</div>
+        </router-link>
+    </div>
+</template>
