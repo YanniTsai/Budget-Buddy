cbf7a02368c5ee534ab2258edd36e746d3adfd23
Yanni Tsai <loveyanni88@gmail.com>
2023-11-25 15:02:13 +0800

    feat: navbar layout of desktop

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid p-0">
+            <a class="navbar-brand d-flex justify-content-center align-items-center" href="#">
+                <i class="bi bi-people-fill"></i>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbar">
+                帳簿管理
+            </div>
+        </div>
+    </nav>
+</template>
