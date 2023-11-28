3f42e5133a71b2d8cd398ccd3f1b7b9fac7b246d
Yanni Tsai <loveyanni88@gmail.com>
2023-11-28 15:40:31 +0800

    feat: add router-link on navbar icon to back to home page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid p-0">
-            <a class="navbar-brand d-flex justify-content-center align-items-center" href="#">
+            <router-link to="/dashboard/ledger" class="navbar-brand d-flex justify-content-center align-items-center">
                 <i class="bi bi-people-fill"></i>
-            </a>
+            </router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
