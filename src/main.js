72cc3ec4017fd485e3661cacc5b3a5473ea647ce
Yanni Tsai <loveyanni88@gmail.com>
2023-12-18 21:27:46 +0800

    import vue-axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -19,6 +21,7 @@ library.add(far, fas)
 
 const app = createApp(App)
 
+app.use(VueAxios, axios)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(store)
 app.use(router)
