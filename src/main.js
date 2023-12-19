5cdb2f368daab6444ca8a71fb08f16a4371c6638
Yanni Tsai <loveyanni88@gmail.com>
2023-12-19 21:12:02 +0800

    fix: remove semicolon

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import Loading from 'vue3-loading-overlay';
-import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
+import Loading from 'vue3-loading-overlay'
+import 'vue3-loading-overlay/dist/vue3-loading-overlay.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
