18c94acf85a8c3f77a5db2fc3b594df61ae6dba5
Yanni Tsai <loveyanni88@gmail.com>
2024-02-10 14:56:12 +0800

    feat: layout of ledger-detail

---
+++ src/components/IncomeModal.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="modal ledger-modal" tabindex="-1" ref="modal">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">新增收入</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="col-md-6 mb-3 d-flex justify-content-center">
+                        <span>2024 年 02 月 10 日 星期六</span>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="currency" class="form-label">收入類別</label>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>類別</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="currency" class="form-label">收入帳戶</label>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>帳戶</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="amount" class="form-label">金額</label>
+                                <input type="text" class="form-control" id="amount" v-model="tempLedger.name">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div>
+                                <label for="describe" class="form-label">備註</label>
+                                <textarea class="form-control" id="describe" rows="8" v-model="tempLedger.description"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn gray-btn" data-bs-dismiss="modal">取消</button>
+                    <button type="button" class="btn blue-btn" @click="$emit('update-ledger', tempLedger)">新增</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Modal from 'bootstrap/js/dist/modal'
+
+export default {
+  props: {
+    ledger: {}
+  },
+  data () {
+    return {
+      modal: {},
+      tempLedger: {}
+    }
+  },
+  watch: {
+    ledger () {
+      this.tempLedger = this.ledger
+    }
+  },
+  methods: {
+    showModal () {
+      this.modal.show()
+    },
+    hideModal () {
+      this.modal.hide()
+    }
+  },
+  mounted () {
+    this.modal = new Modal(this.$refs.modal)
+  }
+}
+</script>
--- src/views/LedgerInfo.vue
+++ src/views/LedgerInfo.vue
@@ -1,3 +1,179 @@
 <template>
-    ledgerinfo
+    <div class="calendar col-10">
+        <div class="calendar-header">
+            <div>
+                <i class="bi bi-caret-left"></i>
+                <span class="mx-3">2024年01月</span>
+                <i class="bi bi-caret-right"></i>
+            </div>
+            <div class="search-btn">
+                <i class="bi bi-search"></i>
+            </div>
+        </div>
+        <div>
+            <table class="table table-bordered w-100">
+                <thead class="table-light">
+                    <tr>
+                        <th>SUN</th>
+                        <th>MON</th>
+                        <th>TUE</th>
+                        <th>WED</th>
+                        <th>THU</th>
+                        <th>FRI</th>
+                        <th>SAT</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(row, rowIndex) in tableData" :key="rowIndex">
+                        <td v-for="(cell, cellIndex) in row" :key="cellIndex">{{ cell }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">紀錄</button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel" ref="offcanvas">
+            <div class="offcanvas-header d-flex justify-content-between">
+                <button type="button" class="btn-close mx-2" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">2024 年 02 月 10 日 星期六</h5>
+                <button class="btn transparent-btn" @click.prevent="showRecordOption = !showRecordOption"><i class="bi bi-plus-circle"></i></button>
+                <div class="recordOption d-flex flex-column" v-if="showRecordOption">
+                    <a href="" @click.prevent="openExpenditureModal"><div class="addRecord-btn btn-border">新增支出</div></a>
+                    <a href="" @click.prevent="openIncomeModal"><div class="addRecord-btn btn-border">新增收入</div></a>
+                    <a href="" @click.prevent="openTransferModal"><div class="addRecord-btn">新增轉帳</div></a>
+                </div>
+            </div>
+            <div class="offcanvas-body">
+                <!-- 有紀錄 -->
+                <div>
+                    <!-- 支出/收入 -->
+                    <div class="mb-3">
+                        <div class="mb-3">
+                            <span>支出：$ 1,500</span>
+                            <span class="ms-3">收入：$ 30,000</span>
+                        </div>
+                        <div>
+                            <!-- 支出 -->
+                            <div class="record-detail py-3 d-flex justify-content-between">
+                                <div class="d-flex">
+                                    <div class="category-icon me-3 d-flex align-items-center">👕</div>
+                                    <div class="d-flex flex-column justify-content-around">
+                                        <div class="title">服飾</div>
+                                        <div class="description">Uniqlo T-shirt *1</div>
+                                    </div>
+                                </div>
+                                <div class="d-flex">
+                                    <div class="text-end d-flex flex-column justify-content-around">
+                                        <div>- $ 1,500</div>
+                                        <div class="payment">中國信託信用卡</div>
+                                    </div>
+                                    <div class="edit-btn d-flex flex-column justify-content-around">
+                                        <button class="btn transparent-btn" @click.prevent="openExpenditureModal"><i class="bi bi-pencil"></i></button>
+                                        <button class="btn transparent-btn" @click.prevent="openDeleteModal"><i class="bi bi-trash3"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- 收入 -->
+                            <div class="record-detail py-3 d-flex justify-content-between">
+                                <div class="d-flex">
+                                    <div class="category-icon me-3 d-flex align-items-center">🧰</div>
+                                    <div class="d-flex flex-column justify-content-around">
+                                        <div class="title">薪水</div>
+                                        <div class="description">10月薪水</div>
+                                    </div>
+                                </div>
+                                <div class="d-flex">
+                                    <div class="text-end d-flex flex-column justify-content-around">
+                                        <div class="income">+ $ 30,000</div>
+                                        <div class="payment">郵局帳戶</div>
+                                    </div>
+                                    <div class="edit-btn d-flex flex-column justify-content-around">
+                                        <button class="btn transparent-btn" @click.prevent="openIncomeModal"><i class="bi bi-pencil"></i></button>
+                                        <button class="btn transparent-btn" @click.prevent="openDeleteModal"><i class="bi bi-trash3"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 轉帳 -->
+                    <div>
+                        <div class="mb-3">
+                            <span>轉帳：$ 3,000</span>
+                        </div>
+                        <div>
+                            <div class="record-detail py-3 d-flex justify-content-between">
+                                <div class="d-flex">
+                                    <div class="category-icon me-3 d-flex align-items-center">🏦</div>
+                                    <div class="d-flex flex-column justify-content-around">
+                                        <div>郵局帳戶</div>
+                                        <div class="description">10月卡費</div>
+                                    </div>
+                                </div>
+                                <div class="d-flex">
+                                    <div class="text-end d-flex flex-column justify-content-around">
+                                        <div>→ 中國信託信用卡</div>
+                                        <div class="transfer">$ 3,000</div>
+                                    </div>
+                                    <div class="edit-btn d-flex flex-column justify-content-around">
+                                        <button class="btn transparent-btn" @click.prevent="openTransferModal"><i class="bi bi-pencil"></i></button>
+                                        <button class="btn transparent-btn" @click.prevent="openDeleteModal"><i class="bi bi-trash3"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 無紀錄 -->
+                <!-- <div class="d-flex justify-content-center">
+                    今天沒有紀錄！
+                </div> -->
+            </div>
+        </div>
+    </div>
+    <!-- <DeleteModal ref="deleteModal" :name="tempLedger.name" @delete-item="deleteLedger"></DeleteModal> -->
+    <ExpenditureModal ref="expenditureModal"></ExpenditureModal>
+    <IncomeModal ref="incomeModal"></IncomeModal>
+    <TransferModal ref="transferModal"></TransferModal>
+    <DeleteModal ref="deleteModal"></DeleteModal>
 </template>
+
+<script>
+import ExpenditureModal from '../components/ExpenditureModal.vue'
+import IncomeModal from '../components/IncomeModal.vue'
+import TransferModal from '../components/TransferModal.vue'
+import DeleteModal from '../components/DeleteModal.vue'
+import Offcanvas from 'bootstrap/js/dist/offcanvas'
+
+export default {
+  data () {
+    return {
+    //   day: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+      tableData: Array.from({ length: 6 }, () => Array(7).fill('')),
+      showRecordOption: false
+    }
+  },
+  components: {
+    ExpenditureModal,
+    IncomeModal,
+    TransferModal,
+    DeleteModal
+  },
+  methods: {
+    openExpenditureModal () {
+      this.$refs.expenditureModal.showModal()
+    },
+    openIncomeModal () {
+      this.$refs.incomeModal.showModal()
+    },
+    openTransferModal () {
+      this.$refs.transferModal.showModal()
+    },
+    openDeleteModal () {
+      // this.tempLedger = { ...item }
+      this.$refs.deleteModal.showModal()
+    }
+  },
+  mounted () {
+    this.offcanvas = new Offcanvas(this.$refs.offcanvas)
+  }
+}
+</script>
